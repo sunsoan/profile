04a140899fa58ae9c6c5a25e1af3f3b5bf644017
DESKTOP-EGVEJGV\yakir <sunsoan@foxmail.com>
2016-10-06 20:37:29 +0800

    增加数据库

--- profile.py
+++ profile.py
@@ -1,9 +1,10 @@
 from flask import Flask,jsonify
-
+import torndb
 app = Flask(__name__)
 
 click_times = {'education': 0, 'ability': 0, 'experience': 0, 'selfEval': 0}
-
+click_times_from_db = {'education': 0, 'ability': 0, 'experience': 0, 'selfEval': 0}
+db = torndb.Connection("127.0.0.1:3306", "myprofile", user="yaya", password="123456")
 
 @app.route('/')
 def hello_world():
@@ -13,12 +14,20 @@ def hello_world():
 @app.route('/status/<clicked_id>')
 def click_status(clicked_id):
     click_times[clicked_id] += 1
+
+    sql_update = "UPDATE status SET click_times = %d WHERE item_name = '%s'"%(click_times[clicked_id],clicked_id)
+    db.update(sql_update)
+
     return ('', 204)
 
 
 @app.route('/get_status_data')
 def get_status_data():
-    return jsonify(click_times)
+    sql_query= "select * from status"
+    results = db.query(sql_query)
+    for row in results:
+        click_times_from_db[row['item_name']] = row['click_times']
+    return jsonify(click_times_from_db)
 
 
 if __name__ == '__main__':
