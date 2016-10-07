dd2de8e294212d5787175df4fe19418f99eebff9
DESKTOP-EGVEJGV\yakir <sunsoan@foxmail.com>
2016-10-08 02:21:07 +0800

    更新数据库，将mysql数据库改为sqlite3

--- profile.py
+++ profile.py
@@ -1,10 +1,34 @@
 from flask import Flask,jsonify
-import torndb
+import sqlite3
+from os import path
+
 app = Flask(__name__)
 
 click_times = {'education': 0, 'ability': 0, 'experience': 0, 'selfEval': 0}
 click_times_from_db = {'education': 0, 'ability': 0, 'experience': 0, 'selfEval': 0}
-db = torndb.Connection("127.0.0.1:3306", "myprofile", user="yaya", password="123456")
+
+basedir = path.abspath(path.dirname(__file__))
+DATABASE = path.join(basedir,'database.db')
+
+
+
+def connect_db():
+    return sqlite3.connect(DATABASE)
+
+
+def init_data():
+    dba = connect_db()
+    sql_query = "select education,ability,experience,selfEval from status"
+    cura = dba.cursor()
+    cura.execute(sql_query)
+    try:
+        click_times['education'], click_times['ability'], click_times['experience'], click_times['selfEval'] = cura.fetchone()
+    except TypeError:
+        print(TypeError.__str__())
+    cura.close()
+    dba.close()
+init_data()
+
 
 @app.route('/')
 def hello_world():
@@ -14,19 +38,26 @@ def hello_world():
 @app.route('/status/<clicked_id>')
 def click_status(clicked_id):
     click_times[clicked_id] += 1
-
-    sql_update = "UPDATE status SET click_times = %d WHERE item_name = '%s'"%(click_times[clicked_id],clicked_id)
-    db.update(sql_update)
-
+    dbac = connect_db()
+    sql_update = "update status set %s = %d "%(clicked_id,click_times[clicked_id])
+    dbac.execute(sql_update)
+    dbac.commit()
+    dbac.close()
     return ('', 204)
 
 
 @app.route('/get_status_data')
 def get_status_data():
-    sql_query= "select * from status"
-    results = db.query(sql_query)
-    for row in results:
-        click_times_from_db[row['item_name']] = row['click_times']
+    dba = connect_db()
+    sql_query= "select education,ability,experience,selfEval from status"
+    cura = dba.cursor()
+    cura.execute(sql_query)
+    try:
+        click_times_from_db['education'],click_times_from_db['ability'], click_times_from_db['experience'], click_times_from_db['selfEval'] = cura.fetchone()
+    except TypeError:
+        pass
+    cura.close()
+    dba.close()
     return jsonify(click_times_from_db)
 
 
